c1803293d079baefe533bf8aae72b072f9d2d3b6
jfaguerre55 <jf_aguerre@hotmail.com>
2024-04-14 15:46:39 -0300

    Add source files for LIS3MDL driver development

---
+++ 02-Source/00-Drivers/Sensor_LIS3MDL/inc/sensor_lis3mdl.h
@@ -0,0 +1,13 @@
+/*
+ * sensor_lis3mdl.h
+ *
+ *  Created on: 14 abr. 2024
+ *      Author: JuanAguerre
+ */
+
+#ifndef SENSOR_LIS3MDL_H_
+#define SENSOR_LIS3MDL_H_
+
+
+
+#endif /* SENSOR_LIS3MDL_H_ */
---
+++ 02-Source/00-Drivers/Sensor_LIS3MDL/src/sensor_lis3mdl.c
@@ -0,0 +1,8 @@
+/*
+ * sensor_lis3mdl.c
+ *
+ *  Created on: 14 abr. 2024
+ *      Author: JuanAguerre
+ */
+
+
