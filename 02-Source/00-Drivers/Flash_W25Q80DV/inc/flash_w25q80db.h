ad5a05046b3eda895b74391ae819326838b73493
jfaguerre55 <jf_aguerre@hotmail.com>
2024-04-16 16:33:50 -0300

    Create basic Read/Write functions for memory ops

--- 02-Source/00-Drivers/Flash_W25Q80DV/inc/flash_w25q80db.h
+++ 02-Source/00-Drivers/Flash_W25Q80DV/inc/flash_w25q80db.h
@@ -9,13 +9,40 @@
 #define FLASH_W25Q80DB_H_
 
 
-#include "spifi_18xx_43xx.h"
+#include "MCU43xx_m4_SPIFI.h"
 
 
-#define		SPIFI_MEM_ADDRESS		(0x14000000)	// UM10503 - Table 444. SPIFI flash memory map
-#define		SPIFI_MEM_SIZE			(0x4000000)		// 64Mbytes
+#define		FLASH_W25Q80_MEM_SIZE			(1*1024*1024)	// 1 Mbyte
+#define		FLASH_W25Q80_BLOCK_SIZE			(64*1024)		// 64 kbyte
+#define		FLASH_W25Q80_SECTOR_SIZE		(4*1024)		// 4 kbyte
+#define		FLASH_W25Q80_PAGE_SIZE			(256)			// bytes
 
 
-// 0x17FF FFFF
+
+
+/**
+ * @brief Memory error data types
+ */
+typedef enum{FLASH_W25Q80L_OK, FLASH_W25Q80_ERROR}	Flash_W25Q80DB_Status_t;
+
+
+
+/**
+ * @brief Flash memory W25Q80DB data type
+ */
+typedef struct{
+
+	uint32_t		head;
+	uint32_t		tail;
+
+
+} Flash_W25Q80DB_t;
+
+
+
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Init(Flash_W25Q80DB_t * mem);
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Write_Array(Flash_W25Q80DB_t * mem, void * buff, size_t buff_size);
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Read_Array(Flash_W25Q80DB_t * mem, uint32_t address, void * buff, size_t buff_size);
+
 
 #endif /* FLASH_W25Q80DB_H_ */
