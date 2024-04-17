08a46a74d17a0c275ab43920c9416ee0ab599a96
jfaguerre55 <jf_aguerre@hotmail.com>
2024-04-16 23:42:47 -0300

    Code Flash memory primitives (Init/R/W/Get)

--- 02-Source/00-Drivers/Flash_W25Q80DV/inc/flash_w25q80db.h
+++ 02-Source/00-Drivers/Flash_W25Q80DV/inc/flash_w25q80db.h
@@ -12,37 +12,55 @@
 #include "MCU43xx_m4_SPIFI.h"
 
 
-#define		FLASH_W25Q80_MEM_SIZE			(1*1024*1024)	// 1 Mbyte
-#define		FLASH_W25Q80_BLOCK_SIZE			(64*1024)		// 64 kbyte
-#define		FLASH_W25Q80_SECTOR_SIZE		(4*1024)		// 4 kbyte
-#define		FLASH_W25Q80_PAGE_SIZE			(256)			// bytes
+#define		FLASH_W25Q80_BASE_ADDRESS		MCU43XX_SPIFI_BASE_ADDRESS	// Hardware base address
 
+#define		FLASH_W25Q80_MEM_SIZE			(1*1024*1024)	// 1 Mbytes
+#define		FLASH_W25Q80_BLOCK_SIZE			(64*1024)		// 64 kbytes
+#define		FLASH_W25Q80_SECTOR_SIZE		(4*1024)		// 4 kbytes
+#define		FLASH_W25Q80_PAGE_SIZE			(256)			// 256 bytes
+
+#define		FLASH_W25Q80_LOT_SIZE			(32)			// 32 bytes
+#define		FLASH_W25Q80_LOT_TOT			(FLASH_W25Q80_SECTOR_SIZE/FLASH_W25Q80_LOT_SIZE)			// 32 bytes
 
 
 
 /**
  * @brief Memory error data types
  */
-typedef enum{FLASH_W25Q80L_OK, FLASH_W25Q80_ERROR}	Flash_W25Q80DB_Status_t;
+typedef enum{FLASH_W25Q80_OK, FLASH_W25Q80_ERROR}	Flash_W25Q80DB_Status_t;
 
 
 
 /**
  * @brief Flash memory W25Q80DB data type
+ * @notes
+ * 		0- El driver está diseñado para utilizar la memoria R/W en pequeños lotes de 32 bytes para guardar datos (ID+VALOR)
+ * 		1- Las operaciones de RW se realizan por lotes de 32 bytes (en cada página de la memoria entran 8 lotes)
+ * 		2- Las operaciones de Write deben estar alineadas en 32 bytes
+ * 		3- La memoria tiene:
+ * 			whole: 1Mb/32bytes = 32k lotes
+ * 			sector:	4k/32bytes = 128 lotes
+ * 		4- La memoria se direcciona por un index del lote (de 0 a 32k-1)
+ * 		5- Si el index del lote que se quiere escribir no coincide el driver de la memoria realiza un erase del lote completo
+ * 		6- La escritura de la memoria debe hacerse de forma progresiva. Desde el index  0 al 32k-1.
+ * 		7- Si se intenta escribir en otro sector distinto al actual se va a borrar el contenido completo del sector
  */
 typedef struct{
 
-	uint32_t		head;
-	uint32_t		tail;
-
+	uint32_t		lot_index;		/* Index of the next lot to be written */
+	uint32_t		sector_index;	/* Current memory sector index */
 
 } Flash_W25Q80DB_t;
 
 
 
 Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Init(Flash_W25Q80DB_t * mem);
-Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Write_Array(Flash_W25Q80DB_t * mem, void * buff, size_t buff_size);
-Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Read_Array(Flash_W25Q80DB_t * mem, uint32_t address, void * buff, size_t buff_size);
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Write_Lot(Flash_W25Q80DB_t * mem, uint32_t address, void * buff );
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Read_Lot(Flash_W25Q80DB_t * mem, uint32_t address, void * buff );
+
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Get_Current_Index(Flash_W25Q80DB_t * mem, uint32_t * lot_index);
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Get_Current_Sector(Flash_W25Q80DB_t * mem, uint32_t * sector_index);
+
 
 
 #endif /* FLASH_W25Q80DB_H_ */
--- 02-Source/00-Drivers/Flash_W25Q80DV/src/flash_w25q80db.c
+++ 02-Source/00-Drivers/Flash_W25Q80DV/src/flash_w25q80db.c
@@ -11,32 +11,74 @@
 
 Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Init(Flash_W25Q80DB_t * mem){
 
+	if(mem == NULL) return FLASH_W25Q80_ERROR;
+
+	mem->lot_index = 0ul;
+	mem->sector_index = FLASH_W25Q80_LOT_TOT+1;	// Starts out of range to force erase the first sector to be written
+
 	MCU_SPIFI_Init();
 	MCU_SPIFI_Reset();
 
-
-	return FLASH_W25Q80L_OK;
+	return FLASH_W25Q80_OK;
 }
 
 
 
-Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Write_Array(Flash_W25Q80DB_t * mem, uint32_t address, void * buff, size_t buff_size){
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Write_Lot(Flash_W25Q80DB_t * mem, uint32_t index, void * buff ){
+
+	if(mem == NULL || index >= FLASH_W25Q80_LOT_TOT) return FLASH_W25Q80_ERROR;
+
+	/* If an index selects a new sector, this sector must be erased */
+	if(mem->sector_index != ((FLASH_W25Q80_LOT_SIZE*index)/FLASH_W25Q80_SECTOR_SIZE)){
+		mem->sector_index = (FLASH_W25Q80_LOT_SIZE*index)/FLASH_W25Q80_SECTOR_SIZE;
+		MCU_SPIFI_Erase_Sector(FLASH_W25Q80_BASE_ADDRESS + FLASH_W25Q80_SECTOR_SIZE*mem->sector_index);
+	}
+
+	mem->lot_index = index;
 
 	MCU_SPIFI_Enter_Command_Mode();
-	MCU_SPIFI_Write_Page(MCU43XX_SPIFI_BASE_ADDRESS+address, buff, buff_size);
+	MCU_SPIFI_Write_Page(FLASH_W25Q80_BASE_ADDRESS + FLASH_W25Q80_LOT_SIZE*index , buff, FLASH_W25Q80_LOT_SIZE);
 
-	return FLASH_W25Q80L_OK;
+	return FLASH_W25Q80_OK;
 }
 
 
-Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Read_Array(Flash_W25Q80DB_t * mem, uint32_t address, void * buff, size_t buff_size){
+
+
+
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Read_Lot(Flash_W25Q80DB_t * mem, uint32_t index, void * buff ){
+
+	if(mem == NULL) return FLASH_W25Q80_ERROR;
 
 	uint32_t i=0;
 	MCU_SPIFI_Enter_Memory_Mode();
 
-	for(i=0; i< buff_size; i++){
-		((uint8_t*)buff)[i] = ((uint8_t*)(MCU43XX_SPIFI_BASE_ADDRESS*address))[i];
+	for(i=0; i< FLASH_W25Q80_LOT_SIZE; i++){
+		((uint8_t*)buff)[i] = ((uint8_t*)(FLASH_W25Q80_BASE_ADDRESS + FLASH_W25Q80_LOT_SIZE*index))[i];
 	}
 
-	return FLASH_W25Q80L_OK;
+	return FLASH_W25Q80_OK;
+}
+
+
+
+
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Get_Current_Index(Flash_W25Q80DB_t * mem, uint32_t * lot_index){
+
+	if(mem == NULL) return FLASH_W25Q80_ERROR;
+
+	*lot_index = mem->lot_index;
+
+	return FLASH_W25Q80_OK;
+}
+
+
+
+Flash_W25Q80DB_Status_t 	Flash_W25Q80DB_Get_Current_Sector(Flash_W25Q80DB_t * mem, uint32_t * sector_index){
+
+	if(mem == NULL) return FLASH_W25Q80_ERROR;
+
+	*sector_index = mem->sector_index;
+
+	return FLASH_W25Q80_OK;
 }
